5ecd5e14c887b7606f9e21a646f6a74967980f92
Hampton <herohamp@users.noreply.github.com>
2016-08-04 21:33:35 -0400

    Update player.js

--- player.js
+++ player.js
@@ -4,9 +4,11 @@ var fps=25; /*frames per seconds*/
 var ucode; /* Var that will hold users code */
 var errorcode; /* Errorcode reported to the user */
 var sprites = {}; /* An Object for all the sprites. Just trust me on the whole object thing please*/
-function Sprite(name, x, y,direction) {
+function Sprite(name, x, y,w,h,direction) {
     this.name = name;
     this.x = x;
+    this.width=w;
+    this.height=h;
     this.y = y;
     this.direction=direction;
     this.walk = function(steps) { /*walking in the direction the sprite is facing*/
